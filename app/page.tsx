871243aee5d3e02e21157d72dfd135da583a40fe
HACHI\huyhu <huyhue2kbg@gmail.com>
2024-02-06 15:01:37 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
-import Image from "next/image";
+'use client'
 import Hero from "./components/Hero";
-import Animate from "./components/Animate";
 import Bubbles from "./components/Bubbles/Bubbles";
+import { useState } from "react";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true)
+
+  const wait = async(ms: number) =>{
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+
+  const LoadingPage = async () => {
+    setLoading(true)
+    try {
+      const time = 5000
+      const startTime = Date.now()
+      await wait(time)
+
+      const endTime = Date.now()
+      const elapsedTime = endTime - startTime
+
+      const remainingTime = Math.max(0, time - elapsedTime)
+      await wait(remainingTime)
+
+      setLoading(false)
+    } catch (error) {
+      console.error('Error loading page:', error)
+      setLoading(false)
+    }
+  };
+
   const container:React.CSSProperties = {
     height:'100vh',
     scrollBehavior:'smooth',
