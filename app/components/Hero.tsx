75208b18d66544b28d28ae6a8b67db0c5e6b42b1
HACHI\huyhu <huyhue2kbg@gmail.com>
2024-02-06 11:43:05 +0700

    update

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,17 +6,23 @@ const Hero = () => {
         <div className="relative h-full w-full">
             <div className="grid grid-cols-[1fr] md:grid-cols-[1fr,1fr]">
                 <div className="hidden md:block w-full h-[100vh]">
-                    <div className="w-full h-full flex justify-center items-center">
-                        <TypeTextEffect Text={['你好，我是辉','Hello, My name Huy', 'Xin chào, Mình là Huy']}/>
-                    </div>
+                   <div className="w-full h-full flex flex-col justify-center items-center">
+                        <div className="flex justify-center items-center">
+                            <TypeTextEffect Text={['Hello, My name Huy','你好，我是辉', 'Xin chào, Mình là Huy']}/>
+                        </div>
+                   </div>
                 </div>
-                <div className="w-full h-[100vh] flex flex-col justify-center items-center">
-                    <div className='block md:hidden my-4'>
-                        <div className="w-full h-full flex justify-center items-center ml-3">
-                            <TypeTextEffect Text={['你好，我是辉','Hello, My name Huy', 'Xin chào, Mình là Huy']}/>
+                <div className="w-full h-[100vh] flex flex-col">
+                    <div className='block md:hidden mx-4 mt-[5rem]'>
+                        <div className="w-full h-full flex flex-col justify-center items-center">
+                            <div className="flex justify-center items-center">
+                                <TypeTextEffect Text={['Hello, My name Huy','你好，我是辉', 'Xin chào, Mình là Huy']}/>
+                            </div>
                         </div>
                     </div>
-                    <Pixel />
+                    <div className="flex justify-center items-center w-full h-full">
+                        <Pixel />
+                    </div>
                 </div>
             </div>
         </div>
