fc9531379a41805dcd3837fd55e31efc041f99a4
DESKTOP-71D54RR\huyhu <01676096561bg@gmail.com>
2024-03-17 09:38:36 +0700

    update

--- app/components/ChatBot/ChatBot.tsx
+++ app/components/ChatBot/ChatBot.tsx
@@ -1,41 +1,96 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import AvatarMe from "./avatarMe"
+
+type BoxChat = {
+    id:number,
+    type:string,
+    text:string
+}
 
 const ChatBot = () => {
     const [valueChat, setValueChat] = useState('')
     const [valueInput, setValueInput] = useState('')
+    const [arrayBox, setArrayBox] = useState<BoxChat[]>([])
     const refInput = useRef<HTMLInputElement>(null)
+
     const sendChat = () => {
-        console.log(refInput?.current?.value)
         setValueChat(refInput?.current?.value??'')
+        const _peo:BoxChat = {
+            id: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1,
+            type:'people',
+            text: refInput?.current?.value??''
+        }
+        setArrayBox([...arrayBox, _peo])
         setValueInput('')
     }
+
+    // useEffect(() => {
+    //     console.log({arrayBox})
+    // },[arrayBox])
+
+    useEffect(() => {
+        setTimeout(() => {
+            const bot:BoxChat = {
+                id: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1,
+                type:'bot',
+                text: 'Hi, Tôi là trợ lí ảo của Huy, hãy đưa tôi một câu hỏi tôi sẽ trả lại bằng một câu trả lời về câu hỏi của bạn.'
+            }
+            setArrayBox([...arrayBox, bot])
+        },500)
+    },[])
 
     const handleKeyPress = (e:any) => {
         if (e.key === 'Enter') {
             setValueChat(refInput?.current?.value??'')
+            const _peo:BoxChat = {
+                id: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1,
+                type:'people',
+                text: refInput?.current?.value??''
+            }
+            setArrayBox([...arrayBox, _peo])
             setValueInput('')
         }
     };
 
     return (
-        <div className="h-[520px] w-[350px] border bg-[#6D6D6D3D] border-[#6D6D6D4D] rounded-[8px]">
+        <div className="h-[calc(100vh-30px)] w-[calc(100vw-10px)] relative md:w-[calc(100vw-100px)] border bg-[#6D6D6D3D] border-[#6D6D6D4D] rounded-[8px]">
             <div className="w-full h-[40px] text-[14px] text-[#6D6D6D] p-2 bg-[#2d2d2d] rounded-tr-[8px] rounded-tl-[8px]">
-                Chat cùng BOT Huy nè... ^^
+                Chat cùng BOT của Huy nè... ^^
             </div>
-            <div className="h-[440px] w-full p-2 text-[13.5px]">
+            <div className="max-h-[calc(100vh-120px)] overflow-x-hidden overflow-y-auto w-full p-2 text-[14px]">
                 {
-                     <div className="flex gap-x-[5px]">
-                     <AvatarMe />
-                     {
-                        valueChat
-                     }
-                     </div>
+                    arrayBox && arrayBox.map((item, idx) => (
+                        <div key={idx} className="w-full h-full">
+                            {
+                                item.type === 'bot' && (
+                                    <div key={Math.random()} className="flex justify-start w-full my-[10px] rounded-[10px]">
+                                        <div className="w-fit break-all h-full bg-gray-950 px-3 py-3 rounded-[10px] max-w-[90%] md:max-w-[650px] lg:max-w-[980px] xl:max-w-[50%]">
+                                        {
+                                            item.text
+                                        }
+                                        </div>
+                                    </div>
+                                )
+                            }
+                            {
+                                item.type === 'people' && (
+                                    <div key={Math.random()} className="flex justify-end w-full my-[10px] rounded-[10px]">
+                                       <div className="w-fit break-all h-full bg-blue-700 px-3 py-3 rounded-[10px] max-w-[90%] md:max-w-[650px] lg:max-w-[980px] xl:max-w-[50%]">
+                                        {
+                                            item.text
+                                        }
+                                       </div>
+                                    </div>
+                                )
+                            }
+                        </div>
+                    ))
                 }
             </div>
-            <div className="relative h-[40px] rounded-br-[8px] rounded-bl-[8px] overflow-hidden bg-[#2d2d2d] shadow-xl w-full">
+            <div className="absolute bottom-0 h-[40px] rounded-br-[8px] rounded-bl-[8px] overflow-hidden bg-[#2d2d2d] shadow-xl w-full">
                 <input
                     ref={refInput}
+                    autoComplete="off"
                     type="text"
                     name="text"
                     value={valueInput}
