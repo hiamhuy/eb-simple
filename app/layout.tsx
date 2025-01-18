23543ecfd9943a802a62aeedcae6ad383a093a6b
HACHI\huyhu <huyhue2kbg@gmail.com>
2025-01-18 14:06:17 +0700

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Chăm sóc sức khỏe",
+  title: "Hàng vạn lời yêu không bằng một chiều anh đưa em đi uống bia.",
+  description:'Đôi khi, tình yêu không nằm ở những lời hoa mỹ hay món quà xa xỉ, mà được gói gọn trong những khoảnh khắc đời thường nhưng chân thật. Một buổi chiều, khi ánh nắng dịu nhẹ đổ dài trên con phố, anh và em cùng nhau ngồi lại, nhấp những ngụm bia mát lạnh, chia sẻ những câu chuyện nhỏ bé nhưng ý nghĩa. Hạnh phúc thật sự là cảm giác bình yên khi có người cùng lắng nghe, cùng thấu hiểu, và cùng tận hưởng những điều giản dị nhất trong cuộc sống. Có lẽ, hàng vạn câu yêu thương cũng không thể diễn tả hết ý nghĩa của một chiều như thế. Vì vậy, đôi khi, chỉ cần có nhau, mọi thứ đều trở nên trọn vẹn.'
 };
 
 export const viewport: Viewport = {
